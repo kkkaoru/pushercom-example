d27a075cdbdf3a07dcee205dd63acd12f484c5c6
kaoru.ohtaka <ockacru+git@gmail.com>
2023-03-23 02:14:44 +0900

    refactor: add page extensions

--- apps/pusher-com-app/next.config.js
+++ apps/pusher-com-app/next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  pageExtensions: ['page.tsx', 'api.ts'],
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
